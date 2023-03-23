101ae0928264d38fb4c5f5c9e756f21d7a824594
Anton Coleman <acoleman@paloaltonetworks.com>
2023-03-23 16:42:34 -0500

    skeleton for config output

---
+++ examples/config_output/main.tf
@@ -0,0 +1,6 @@
+#data "sase_shared_infrastructure_settings_list" "this" {
+#}
+#
+#output "sase_shared_infrastructure_settings_list" {
+#  value = data.sase_shared_infrastructure_settings_list.this
+#}
